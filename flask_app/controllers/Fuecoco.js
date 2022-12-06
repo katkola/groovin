7aef14d21d0b3b65b9453dd746c1cbba8b63614b
katkola <katkola@gmail.com>
2022-12-06 00:21:43 -0500

    new commit

---
+++ flask_app/controllers/Fuecoco.js
@@ -0,0 +1,12 @@
+// 👇️ named export
+export class Fuecoco {
+    constructor() {
+        this.position = (0,0);
+        this.flip = False;
+        this.direction= (0,0);
+    }
+
+    getLeft(){
+        return
+    }
+}
